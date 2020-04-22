0c8ecbc971661e1957756b2f7a65138dd7fb5f76
Sylvain Lefebvre <Sylvain.Lefebvre@inria.fr>
2020-04-22 10:00:08 +0200

    added option for zip

--- mingw32/compile_silice.sh
+++ mingw32/compile_silice.sh
@@ -6,7 +6,7 @@ cd BUILDS
 pacman -S --noconfirm --needed unzip wget perl
 
 wget -c https://download.java.net/java/GA/jdk14.0.1/664493ef4a6946b186ff29eb326336a2/7/GPL/openjdk-14.0.1_windows-x64_bin.zip
-unzip openjdk-14.0.1_windows-x64_bin.zip
+unzip -o openjdk-14.0.1_windows-x64_bin.zip
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PATH=$PATH:$DIR/jdk-14.0.1/bin/
