c70f7aefd87d9b7b443288883b5fc1b0a59591ef
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2020-08-15 01:17:45 +0200

    exe striping, minor tweaks

--- mingw32/package.sh
+++ mingw32/package.sh
@@ -8,4 +8,4 @@ do
 done
 
 cd ..
-zip -r fpga-binutils.zip ./mingw32/bin/ ./mingw32/lib/ ./mingw32/include/ ./mingw32/libexec/ ./mingw32/share/ ./mingw32/riscv32-unknown-elf/
+zip -r fpga-binutils-32.zip ./mingw32/bin/ ./mingw32/lib/ ./mingw32/include/ ./mingw32/libexec/ ./mingw32/share/ ./mingw32/riscv32-unknown-elf/
--- mingw64/package.sh
+++ mingw64/package.sh
@@ -8,4 +8,4 @@ do
 done
 
 cd ..
-zip -r fpga-binutils-x64.zip ./mingw64/bin/ ./mingw64/lib/ ./mingw64/include/ ./mingw64/libexec/ ./mingw64/share/ ./mingw64/riscv32-unknown-elf/
+zip -r fpga-binutils-64.zip ./mingw64/bin/ ./mingw64/lib/ ./mingw64/include/ ./mingw64/libexec/ ./mingw64/share/ ./mingw64/riscv32-unknown-elf/
